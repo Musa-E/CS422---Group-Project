16f3f6bde1854c338b8dbc5cc4f02fde0f3146f9
WesKw <kwiecinskiwesley@gmail.com>
2024-11-06 16:59:51 -0600

    Updated yarn to use softbody library instead with some tweaks

--- catandyarn/Yarn/yarn.gd
+++ catandyarn/Yarn/yarn.gd
@@ -1,54 +1,61 @@
-extends RigidBody2D
+extends Node2D
 
-# todo:: Help me
-@export_range(0, 2) var rope_segment_length: float
+@export var rope_segment_scene: PackedScene = null
+# todo:: Get rope working then adjust the 
+@export var rope_segment_length: float
 @export_range(0, 1) var rope_segment_height: float
-@onready var collision_circle: CircleShape2D = $CollisionShape2D.shape
-@onready var joint: Node2D = $"../Joint"
-@onready var velocity_dir: Node2D = $"../VelocityDirection"
-@onready var string_segments := $StringSegments
-@onready var rope_segments: Array[RigidBody2D] = []
+@export var joint_parent: Node2D
+@onready var initial_rope_segment: SoftBody2D = $"Joint/SoftBody2D"
+@onready var initial_joint: PinJoint2D = $"Joint/PinJoint2D"
+@onready var rope_segments: Array[SoftBody2D] = []
 @onready var debug_segments: int = 0
+@onready var yarn_body_component: RigidBody2D = $"Yarn"
 
+var last_rotation = 0
 
-var last_rotation = Vector2.ZERO
+func _ready() -> void:
+	# set initial rope segment
+	rope_segments.append(initial_rope_segment)
+	initial_joint.node_a = yarn_body_component.get_path() # we are on the yarn
+	initial_joint.node_b = initial_rope_segment.get_node("Bone-0").get_path()
 
-func create_new_rope_segment(size: Vector2) -> RigidBody2D:
+func create_new_rope_segment() -> void:
 	# define the new box
-	var rope_segment := RigidBody2D.new()
-	#var rope_polygon := 
-	var rope_box := CollisionShape2D.new()
-	var shape := RectangleShape2D.new()
-	shape.size = size
-	rope_box.shape = shape
-	#rope_segment.add_child(rope_box)
-	return rope_segment
+	var rope_segment := rope_segment_scene.instantiate()
+	var next_pinjoint := PinJoint2D.new()
+	joint_parent.add_child(next_pinjoint)
+	joint_parent.add_child(rope_segment)
+	rope_segments.append(rope_segment)
+	next_pinjoint.global_position = rope_segments[-2].get_node("Bone-3").global_position
+	rope_segment.global_position = next_pinjoint.global_position
+	next_pinjoint.node_a = rope_segments[-2].get_node("Bone-3").get_path() # guaranteed to exist
+	next_pinjoint.node_b = rope_segments[-1].get_node("Bone-0").get_path()
+	rope_segments[-1].apply_impulse(rope_segments[-2].get_node("Bone-3").linear_velocity, rope_segments[-2].global_position)
 	
 ## Process physics on the yarn. When this method is called, a new segment of
 ## string should be attached to a the yarn.
 func _physics_process(delta: float) -> void:
 	# PinJoint follows behind the yarn based on the velocity
-	var interp_location = linear_velocity.normalized()
-	var interp_angle = lerp_angle(joint.rotation, interp_location.angle() - PI/2, 0.5)
-	joint.rotation = interp_angle
-	joint.global_position = global_position
-	
-	velocity_dir.rotation = linear_velocity.normalized().angle()
-	velocity_dir.global_position = global_position
+	# editors note: Turns out we don't really even need to do this anyway.
+	#var interp_location = yarn_body_component.linear_velocity.normalized()
+	#var interp_angle = lerp_angle(joint_parent.rotation, interp_location.angle() - PI/2, 0.5)
+	#joint.rotation = interp_angle
+	#joint.global_position = global_position
+	#
+	#velocity_dir.rotation = linear_velocity.normalized().angle()
+	#velocity_dir.global_position = global_position
 	
 	# todo:: The math here is not exactly what I'm looking for, the segments are
 	#        too small / there are not enough segments being created for how fast
 	#        the ball is rolling
-	var angle = transform.x.normalized().angle_to(last_rotation.normalized())
-	var arc_len = angle * 2 * PI * collision_circle.radius
-	var createNewSegment = (arc_len >= rope_segment_length)
+	var angle = yarn_body_component.rotation
+	var arc_len = abs((angle - last_rotation)) * 2 * PI
+	print(angle - last_rotation)
+	var createNewSegment = (arc_len >= rope_segment_length * 2)
 
 	if createNewSegment:
 		debug_segments += 1
 		print("Created new rope segment ", debug_segments)
-		last_rotation = transform.x.normalized()
-		var new_joint := PinJoint2D.new()
-		var segment = create_new_rope_segment(Vector2(rope_segment_length, rope_segment_height))
-		#string_segments.add_child(segment)
-		#rope_segments.append(segment)
+		last_rotation = yarn_body_component.rotation
+		create_new_rope_segment()
 	
